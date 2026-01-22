d163b9cc57493f1569dba54df7a6a31daf73e65d
Astrocraftgaming <debasree17016@gmail.com>
2026-01-22 17:37:32 +0530

    Added Calculator

---
+++ Calculator.py
@@ -0,0 +1,41 @@
+
+
+Operator = "0"
+num1 = "0"
+num2="0"
+while num1 == "0":
+    try:
+        num1 = float(input("Enter the first number:"))
+    except ValueError:
+        print("Only give numbers please!")
+while num2 == "0":
+    try:
+        num2 = float(input("Enter the second number:"))
+    except ValueError:
+        print("Only give numbers please!")
+while Operator != "+" or "-" or "/" or "*":
+    Operator = str(input("Enter the operator(+,-,/,*):"))
+    if Operator == "+":
+            result = num1+num2
+            print(f"The result is {result}")
+            break
+    elif Operator == "-":
+            result = num1 - num2
+            print(f"The result is {result}")
+            break
+    elif Operator == "/":
+        try:
+            result=num1/num2
+            print(f"The result is {result}")
+            break
+        except ZeroDivisionError:
+            print("The result is ∞")
+            break
+    elif Operator == "*":
+            result = num1 * num2
+            print(f"The result is {result}")
+            break
+    else:
+                print(f"{Operator} is not a valid operator")
+                break
+
