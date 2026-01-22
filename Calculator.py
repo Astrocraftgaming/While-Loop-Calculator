256fb051d026206b61e28cc7d41825f9876381cd
Astrocraftgaming <debasree17016@gmail.com>
2026-01-23 01:04:35 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,72 @@
-k
+import math
+Operator = "0"
+num1 = "0"
+num2="0"
+while num1 == "0":
+    try:
+        num1 = float(input("Enter the first number:"))
+    except ValueError:
+        print("Only give numbers please!")
+while num2 == "0":
+    try:
+        num2 = float(input("Enter the second number(press 1 if you wanna find the square root):"))
+    except ValueError:
+        print("Only give numbers please!")
+if num2==1:
+        result = math.sqrt(num1)
+        print(f"The result is {result}")
+else:
+ while Operator != "+" or "-" or "/" or "*":
+    Operator = str(input("Enter the operator(+,-,/,*,%,^):"))
+    if Operator == "+":
+       while num2=="0":
+                result = num1+num2
+                print(f"The result is {result}")
+                break
+    elif Operator == "-":
+               result = num1-num2
+               print(f"The result is {result}")
+               break        
+    elif Operator == "/":
+      while num2=="0":
+        try:
+            result=num1/num2
+            print(f"The result is {result}")
+            break
+        except ZeroDivisionError:
+            print("The result is ∞")
+            break
+    elif Operator == "*":
+        while num2=="0":
+            try:
+                 num2 = float(input("Enter the second number:"))
+            except TypeError:
+                print("Only give numbers please!")
+            except ValueError:
+                print("Only give numbers please!")
+            try:
+                result = num1 * num2
+                print(f"The result is {result}")
+            finally:
+                break
+    elif Operator == "%":
+        while num2=="0":
+            try:
+                 num2 = float(input("Enter the second number:"))
+            except TypeError:
+                print("Only give numbers please!")
+            except ValueError:
+                print("Only give numbers please!")
+            try:
+                result = num1 % num2
+                print(f"The result is {result}")
+            finally:
+                break
+            
+    elif Operator == "^":
+                 result = num1 ** num2
+                 print(f"The result is {result}")
+                 break
+    else:
+                print(f"{Operator} is not a valid operator")
+
